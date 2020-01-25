a630492802d46f71ae9d23dff082780be8a8394c
Emilie-thp <emilie.marjollet@orange.fr>
2020-01-25 17:16:40 +0100

    Added TribeMembers controller, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+	root 'tribe_members#index'
+  
+  resources :tribe_members, only: [:index, :new, :create]
+
 end
