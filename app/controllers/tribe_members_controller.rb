9390b29fcd0e93d944125fc31414556823182940
Emilie-thp <emilie.marjollet@orange.fr>
2020-01-26 19:08:56 +0100

    Added the search method for ancestors also

--- app/controllers/tribe_members_controller.rb
+++ app/controllers/tribe_members_controller.rb
@@ -1,11 +1,17 @@
 class TribeMembersController < ApplicationController
   
   def index
-  	puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-  	puts params 
-		if params[:search]  	
-			@tribe_members = TribeMember.where("name LIKE ? OR surname LIKE ? OR birthdate LIKE ?", "%"+params[:search]+"%", "%"+params[:search]+"%", "%"+params[:search]+"%").page(params[:page])
+
+		if params[:search] 
+      #result1 regroups members whose name or surname or birthdate is composed of the searched string
+			result1 = TribeMember.where("name LIKE ? OR surname LIKE ? OR birthdate LIKE ?", "%"+params[:search]+"%", "%"+params[:search]+"%", "%"+params[:search]+"%")
+      #result2 regroups members whose ancestor's name or surname is composed of the searched string
+      result2 = TribeMember.where(ancestor: ancestor_ids(params[:search]))
+      #final_result is combination of result1 and result2
+      final_result = result1.or(result2) 
+      @tribe_members = final_result.page(params[:page])
   	else
+      #use of .page(params[:page]) for the pagination gem 'kaminari'
 	  	@tribe_members = TribeMember.page(params[:page])
 	  end
   end
@@ -13,8 +19,15 @@ class TribeMembersController < ApplicationController
   def new
   end
 
+  private 
+
   def tribe_members_params
   	params.require(:tribe_member).permit(:name, :surname, :birthdate, :ancestor, :longitude, :latitude)
 	end
+
+  #to find ids of ancestors from their name or surname
+  def ancestor_ids(params)
+    return TribeMember.where("name LIKE ? OR surname LIKE ?", "%"+params+"%", "%"+params+"%").ids
+  end
 
 end
