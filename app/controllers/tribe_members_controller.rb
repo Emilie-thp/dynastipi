9ee237437f97a98888ec9b3e949ab48d16b098ab
Emilie-thp <emilie.marjollet@orange.fr>
2020-01-26 13:48:09 +0100

    Added search method in controller

--- app/controllers/tribe_members_controller.rb
+++ app/controllers/tribe_members_controller.rb
@@ -1,9 +1,20 @@
 class TribeMembersController < ApplicationController
   
   def index
-  	@tribe_members = TribeMember.page(params[:page])
+  	puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+  	puts params 
+		if params[:search]  	
+			@tribe_members = TribeMember.where("name LIKE ? OR surname LIKE ? OR birthdate LIKE ?", "%"+params[:search]+"%", "%"+params[:search]+"%", "%"+params[:search]+"%").page(params[:page])
+  	else
+	  	@tribe_members = TribeMember.page(params[:page])
+	  end
   end
 
   def new
   end
+
+  def tribe_members_params
+  	params.require(:tribe_member).permit(:name, :surname, :birthdate, :ancestor, :longitude, :latitude)
+	end
+
 end
