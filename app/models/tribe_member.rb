7597545d28d20a9ff548a56b2620e1530d8272cd
Emilie-thp <emilie.marjollet@orange.fr>
2020-01-29 22:17:49 +0100

    Changed message validation for ancestor

--- app/models/tribe_member.rb
+++ app/models/tribe_member.rb
@@ -3,7 +3,7 @@ class TribeMember < ApplicationRecord
 	validates :name, presence: { message: "Veuillez indiquer un nom." }, length: { maximum: 20, too_long: "Le nom doit faire 20 caractères maximum" }
 	validates :surname, presence: { message: "Veuillez indiquer un prénom." }, length: { maximum: 20, too_long: "Le prénom doit faire 20 caractères maximum" }
 	validates :birthdate, presence: { message: "Veuillez indiquer une date de naissance." }
-	validates :ancestor, presence: { message: "Veuillez choisir un numéro d'ancêtre." }, numericality: { only_integer: true, greater_than: 0 }
+	validates :ancestor, presence: { message: "Veuillez choisir un numéro d'ancêtre." }, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: self.all.length }
 	validates :latitude, presence: true
 	validates :longitude, presence: true 
 end
