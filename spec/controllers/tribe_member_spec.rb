ef1998d31073196f09ee6982e8ffcb191b05f6dc
Emilie-thp <emilie.marjollet@orange.fr>
2020-01-30 11:38:55 +0100

    Added 2 tests for search method

--- spec/controllers/tribe_member_spec.rb
+++ spec/controllers/tribe_member_spec.rb
@@ -22,4 +22,22 @@ RSpec.describe TribeMembersController, type: :controller do
 
 	end
 
+  describe "search method" do
+	  
+		context "with a precise params" do
+			it "finds a searched tribe_member by name" do
+		    tribe_member = TribeMember.create(name: "TestName", surname: "Bonjour", birthdate: "1794-02-12", ancestor: 2300, latitude: "2.34555", longitude: "45.2345")
+		    @result = TribeMember.search("TestName")
+		    expect(@result).to eq([tribe_member])
+		  end
+
+		  it "finds a searched tribe_member by date" do
+		    tribe_member = TribeMember.create(name: "TestYear", surname: "Bonjour", birthdate: "2002-02-12", ancestor: 2300, latitude: "2.34555", longitude: "45.2345")
+		    @result = TribeMember.search("2002")
+		    expect(@result).to eq([tribe_member])
+		  end
+		end
+
+	end
+
 end
